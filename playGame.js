3ef79e2aac97aad9fa6a16311fd3c9bdc29453cb
christian <106384215+Theplanter@users.noreply.github.com>
2024-08-29 12:49:29 -0400

    Add winning conditions function and game winning statements

--- playGame.js
+++ playGame.js
@@ -1,3 +1,5 @@
+let userScore=1, comScore =1
+
 function getRandomhand(){
     //this creates a random number between 1 and 3 
     let comput = Math.floor(Math.random() * (3 - 1 +1)+1)
@@ -8,13 +10,15 @@ function getRandomhand(){
     }else if(comput===2){
         return "paper"
     }else{
-        return "scissors"
+        return "scissor"
     }
 
 
 }
 function getUser(){
-    let user = prompt("Type Rock, Paper, or Scissors, to see if you can beat the computer");
+    /*this prompt the user until there is a correct input
+    when the correct one is inputted then the result is returned*/ 
+    let user = prompt("Type Rock, Paper, or Scissors, to see if you can beat the computer").toLowerCase();
     let thing = false
     while(thing == false)
         {
@@ -27,9 +31,44 @@ function getUser(){
     }
 }
 }
+function playGame(userI, comI){
+/*this compares the user and the computer input the who won the round
+whoever wins gets their scores increased*/ 
+    if(userI==="rock" && comI==="scissor"||userI==="paper" && comI==="rock" ||
+        userI==="scissor" && comI==="paper"){
+            console.log(`user won that round`)
+            userScore++
+        }else if(comI===userI){
+                
+                console.log("there was a tie")            
+        }else{
+            console.log(`computer won that round`)
+            comScore++
+        }
 
+}
+/*the while loop stays active until the counter hits 5
+while the loop is active it actives all the functions*/
+let counter =1
+while(counter<=5)
+    {
+        console.log(`round ${counter}`)
 let com = getRandomhand()
 console.log(com)
 
 let user = getUser()
-console.log(user)
+console.log(userScore)
+playGame(user , com);
+counter++
+}
+/*the if statement checks to see who has won the whole game*/
+if(userScore>comScore){
+    console.log("User as won the game")
+
+}else if(comScore>userScore)
+    {
+    console.log("computer as won the game")
+
+}else{
+    console.log("the game is a draw")
+}
